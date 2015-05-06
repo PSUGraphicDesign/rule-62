b75029c034cfc9e5946cc82943ad57e15173ce8c
jblanck <blanck@pdx.edu>
2015-05-06 10:57:59 -0700

    update blueprint
    this is for the news and home updates…get excited.

---
+++ site/blueprints/update.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: update
+pages: 
+  hide: true
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  date: 
+    label: Date Posted
+    type: date
+  text:
+    label: Text
+    type:  textarea
---
+++ site/blueprints/updates.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: updates
+pages: update
+files: 
+  hide: true
+fields:
+  title:
+    label: Title
+    type:  text
+
