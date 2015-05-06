fcc2c62ae72ebcfe09847b72225671d10814f546
Sonny Renna <sonnyrenna@Sonnys-MacBook-Pro.local>
2015-05-06 11:33:11 -0700

    shop blue prints
    items/item/php files

---
+++ site/blueprints/item.php
@@ -0,0 +1,17 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Item
+pages: 
+  hide: true
+files: true
+fields:
+  title: 
+    label: Title
+    type:  text
+  title:
+    label: item name
+    type: text
+  number:
+    label: number
+    type: number
+
---
+++ site/blueprints/items.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Items
+pages: item
+  
+files: 
+  hide: true
+fields:
+  title: 
+    label: title
+    type: text
