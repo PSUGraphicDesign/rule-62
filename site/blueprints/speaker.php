6c579539dd725815ef2fbfc7dc2c6b306529a714
AngeliaFern2008 <angelia3@pdx.edu>
2015-05-06 11:33:13 -0700

    speaker blueprints
    speakers/speaker php files

---
+++ site/blueprints/speaker.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Speaker
+pages: 
+  hide: true
+files: true
+fields:
+  title:
+    label: Name
+    type:  text
+  text:
+    label: Description
+    type:  textarea
+  text:
+    label: Group
+    type: textarea
---
+++ site/blueprints/speakers.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: speakers
+pages: speaker
+files:
+  hide: true
+fields:
+  title:
+    label: Title
+    type:  text
