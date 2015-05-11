e8e1ec8e1c6a7c52fcb590dbece0d88ae43fdb74
Sonny Renna <sonnyrenna@Sonnys-MacBook-Pro.local>
2015-05-11 09:53:57 -0700

    contact page blueprint

---
+++ site/blueprints/contact.php
@@ -0,0 +1,22 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Contact
+pages: 
+  hide: true
+files: 
+  hide: true
+fields:
+  name:
+    label: Name
+    type: text
+  email:
+    label: Email
+    type: email
+    autocomplete: false
+  phone:
+    label: Phone
+    type: tel
+
+
+
+
