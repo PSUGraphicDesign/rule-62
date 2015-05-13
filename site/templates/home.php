474228e31ddfa407354f06c26a13874460dde5d6
August Miller <hello@gusmiller.com>
2015-05-12 23:32:09 -0700

    News Intro

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,6 +5,14 @@
   <section class="content">
     <article>
       <div class="column three-quarters main">
+        <div class="news-intro">
+          <div class="date">
+            <?= $update->date('D, F j, Y') ?>
+          </div>
+          <h2>
+            New Event!
+          </h2>
+        </div>
         <? snippet('news-post', ['post' => $update]) ?>
       </div>
       <div class="column quarter sidebar">
