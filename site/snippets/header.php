5b684b8c4a6b69b56eda43e086908f1681075f73
August Miller <hello@gusmiller.com>
2015-05-12 23:02:28 -0700

    Header links

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,3 +11,22 @@
 
 </head>
 <body class="<?= R62::body_classes() ?>">
+  <header>
+    <section>
+      <article>
+        <div class="column half">
+          <h1><?= $site->title()->htm() ?></h1>
+        </div>
+        <div class="column half">
+          <nav>
+            <ul>
+              <li><?= R62::link_for(page('home'), 'Home') ?></li>
+              <li><?= R62::link_for(page('news'), 'News') ?></li>
+              <li><?= R62::link_for(page('testimonials'), 'Testimonials') ?></li>
+              <li><?= R62::link_for(page('speakers'), 'Speakers') ?></li>
+            </ul>
+          </nav>
+        </div>
+      </article>
+    </section>
+  </header>
