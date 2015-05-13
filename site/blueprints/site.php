dd385d2fc20f1e6a9bf2dad93e3eeddfde01facb
August Miller <hello@gusmiller.com>
2015-05-12 23:18:46 -0700

    Site blueprint, with copyright information

---
+++ site/blueprints/site.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Site
+pages: default
+files: false
+fields:
+  title:
+    label: Product Name
+    type: text
+  copyright:
+    label: Copyright Information
+    type: textarea
+    buttons: false
