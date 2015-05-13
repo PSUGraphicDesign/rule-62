c40a6afbf3db7844770ee1924fad73d5de03de86
August Miller <hello@gusmiller.com>
2015-05-12 23:19:02 -0700

    Use copyright info in footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,11 @@
-</body>
+    <footer>
+      <section>
+        <article>
+          <div class="column full">
+            <?= $site->copyright()->kirbytext() ?>
+          </div>
+        </article>
+      </section>
+    </footer>
+  </body>
 </html>
