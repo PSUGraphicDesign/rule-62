82e211faef83329efc13b96d2e4fda6bb6a558fb
August Miller <hello@gusmiller.com>
2015-05-12 23:19:14 -0700

    Short echo tags in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
 
   <?= css("assets/css/rule-62.css") ?>
 
