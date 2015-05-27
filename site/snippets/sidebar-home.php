cba96614078988d4dedeccdcdabe42fa4c779626
AngeliaFern2008 <angelia3@pdx.edu>
2015-05-26 17:11:05 -0700

    adding sidebar elements

--- site/snippets/sidebar-home.php
+++ site/snippets/sidebar-home.php
@@ -1,4 +1,4 @@
-
+<div class="sidebar-home">
 <nav class="sidebar">
   <img src="<?= $site->url() ?>/assets/images/coin.png">
   <ul>
@@ -7,7 +7,12 @@
     <li><a href="">Location</li></a>
   </ul>
 </nav>
+</div>
 
 <div class="email-sign-up">
-  <p>Sign up for the Rule62 Newsletter!</p>
+  <h4>Sign up for the Rule62 Newsletter!</h4>
+</div>
+
+<div class="social-media">
+  <h3>Follow Us!<a href="<?= $site->url() ?>"><img src="<?= $site->url() ?>/assets/images/facebook-white.png" height="40" width="40"></a><a href="<?= $site->url() ?>"><img src="<?= $site->url() ?>/assets/images/twitter-white.png" height="40" width="40"></a></h3>
 </div>
