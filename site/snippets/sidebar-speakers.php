78745d3967d082a86cab50fda8625494e908968a
AngeliaFern2008 <angelia3@pdx.edu>
2015-05-28 11:05:50 -0700

    speakers sidebar beginnings

--- site/snippets/sidebar-speakers.php
+++ site/snippets/sidebar-speakers.php
@@ -1,1 +1,6 @@
-Speaker Nav
+<div class="sidebar-speakers">
+  <div class="search-bar">
+    <img src="<?= $site->url() ?>/assets/images/search-icon.png" height="25px" width="25px">
+  </div>
+
+</div>
