0c56975d19eadead10b9a43208f2c0acb8bbe748
jblanck <blanck@pdx.edu>
2015-06-01 11:45:55 -0700

    Linked in email sign up with mail chimp
    needs to get styled

--- site/snippets/sidebar-home.php
+++ site/snippets/sidebar-home.php
@@ -11,6 +11,26 @@
 
 <div class="email-sign-up">
   <h4>Sign up for the Rule62 Newsletter!</h4>
+  <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//rulesixtytwo.us11.list-manage.com/subscribe/post?u=7c238886b8be00df1b35ebfd2&amp;id=1e81cf603d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+  
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_7c238886b8be00df1b35ebfd2_1e81cf603d" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 </div>
 
 <div class="social-media">
