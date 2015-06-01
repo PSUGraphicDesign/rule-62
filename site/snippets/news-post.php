8da825eefcbca4ce170060e6739dfda3c989db28
jblanck <blanck@pdx.edu>
2015-06-01 10:26:20 -0700

    Title first

--- site/snippets/news-post.php
+++ site/snippets/news-post.php
@@ -1,10 +1,11 @@
 <div class="post <?= $post->uid() ?>">
   <div class="post-meta">
+    <h3><?= html::a($post->url(), $post->title()) ?></h3>
     <div class="date">
       <?= $post->date('D, F j, Y') ?>
     </div>
   </div>
-  <h3><?= html::a($post->url(), $post->title()) ?></h3>
+ 
   <div class="post-content">
     <?= $post->text()->kirbytext() ?>
   </div>
