45de4ae7076b5d304d5f48bd19dbf61182a56145
Sonny Renna <sonnyrenna@Sonnys-MacBook-Pro.local>
2015-06-01 10:35:08 -0700

    H3 outside of post meta in blog posts

--- site/snippets/news-post.php
+++ site/snippets/news-post.php
@@ -1,6 +1,7 @@
 <div class="post <?= $post->uid() ?>">
+  <h3><?= html::a($post->url(), $post->title()) ?></h3>
+  
   <div class="post-meta">
-    <h3><?= html::a($post->url(), $post->title()) ?></h3>
     <div class="date">
       <?= $post->date('D, F j, Y') ?>
     </div>
