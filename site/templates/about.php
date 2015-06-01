fd8c6af9212eb22a81dea780f17368e9b07e33b3
Sonny Renna <sonnyrenna@Sonnys-MacBook-Pro.local>
2015-06-01 10:32:59 -0700

    About blueprint and template stub

---
+++ site/templates/about.php
@@ -0,0 +1,17 @@
+<? snippet('header') ?>
+
+<main>
+  <section class="hero"></section>
+  <section class="content">
+    <article>
+      <div class="column two-thirds main">
+        Temporary About!
+      </div>
+      <div class="column third sidebar">
+        <? snippet('sidebar-home') ?>
+      </div>
+    </article>
+  </section>
+</main>
+
+<? snippet('footer') ?>
