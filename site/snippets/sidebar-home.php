2f41f7f17f495098d88c1a86c2fd8f148249efac
August Miller <hello@gusmiller.com>
2015-06-08 18:53:55 -0700

    Real URLs for about links
    These are going away, though

--- site/snippets/sidebar-home.php
+++ site/snippets/sidebar-home.php
@@ -2,9 +2,9 @@
   <nav>
     <img src="<?= $site->url() ?>/assets/images/coin.png">
     <ul class="column-group">
-      <li class="column third"><a href="http://localhost:8000/about">About</li></a>
-      <li class="column third"><a href="http://localhost:8000/about">Contact</li></a>
-      <li class="column third"><a href="http://localhost:8000/about">Location</li></a>
+      <li class="column third"><a href="<?= page('about')->url() ?>">About</li></a>
+      <li class="column third"><a href="<?= page('about')->url() ?>#contact">Contact</li></a>
+      <li class="column third"><a href="<?= page('about')->url() ?>#location">Location</li></a>
     </ul>
   </nav>
 </div>
@@ -21,7 +21,7 @@
 <div class="signin" id="mc_embed_signup">
 <form action="//rulesixtytwo.us11.list-manage.com/subscribe/post?u=7c238886b8be00df1b35ebfd2&amp;id=1e81cf603d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-  
+
   <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_7c238886b8be00df1b35ebfd2_1e81cf603d" tabindex="-1" value=""></div>
